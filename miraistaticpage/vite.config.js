c55e8561a59d02c3e682b249dd63b84bdfe63afc
Francis Daniel Genese <tristandivider8@gmail.com>
2026-05-18 17:27:18 +0800

    anotheerrrr

--- miraistaticpage/vite.config.js
+++ miraistaticpage/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Mirai---TechEx-Hackathon-Transforming-Enterprise-Through-AI-May-11-20-2026-/',
+  base: '/',
 })
